354f83cb3527399ce3123ba094b01619912e2de1
Dinesh Rajineni <dinesh.r@techaspect.com>
2018-09-27 11:28:10 +0530

    Added JavaScript Source Code for Challenge Merge Point of Two Lists

--- Day_5/mergepointoftwolists.js
+++ Day_5/mergepointoftwolists.js
@@ -1,4 +1,68 @@
-/*
+/* Simple Hello World in Node.js */
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        console.log(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            console.log(sep);
+        }
+    }
+}
+    /*
     Find merge point of two linked lists
     Note that the head may be 'null' for the empty list.
     Node is defined as
@@ -14,7 +78,7 @@
 function findMergeNode(headA, headB) {
     
     var current1=headA;
-    var currrent2=headB;
+    var current2=headB;
     
      while(current1 != current2){
              if(current1.next == null){
@@ -35,4 +99,54 @@ function findMergeNode(headA, headB) {
     }
     return current2.data;
 
+}
+    function main() {
+  
+
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const index = parseInt(readLine(), 10);
+
+        const llist1Count = parseInt(readLine(), 10);
+
+        let llist1 = new SinglyLinkedList();
+
+        for (let i = 0; i < llist1Count; i++) {
+            const llist1Item = parseInt(readLine(), 10);
+            llist1.insertNode(llist1Item);
+        }
+      
+      	const llist2Count = parseInt(readLine(), 10);
+
+        let llist2 = new SinglyLinkedList();
+
+        for (let i = 0; i < llist2Count; i++) {
+            const llist2Item = parseInt(readLine(), 10);
+            llist2.insertNode(llist2Item);
+        }
+      
+      	var ptr1 = llist1.head;
+        var ptr2 = llist2.head;
+
+        for (var i = 0; i < llist1Count; i++) {
+          if (i < index) {
+            ptr1 = ptr1.next;
+          }
+        }
+
+        for (var i = 0; i < llist2Count; i++) {
+          if (i != llist2Count-1) {
+            ptr2 = ptr2.next;
+          }
+        }
+
+        ptr2.next = ptr1;
+
+        let result = findMergeNode(llist1.head, llist2.head);
+
+        console.log(result + "\n");
+    }
+
+   
 }
