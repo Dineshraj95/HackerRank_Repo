2e7db6b97518bd71ee9c84f2582f925589b5862a
Dinesh Rajineni <dinesh.r@techaspect.com>
2018-10-06 13:38:36 +0530

    Added JavaScript Source Code for Challenge Append and Delete

---
+++ Day_11/appendanddelete.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the appendAndDelete function below.
+function appendAndDelete(s, t, k) {
+    
+     var i=0;
+        for(;i<s.length&&i<t.length;i++)
+            if(s[i]!=t[i])
+                break;
+        var len=s.length+t.length-i*2;
+        if(k<len)
+            return "No";
+        else if(k>=s.length+t.length||(k-len)%2==0)
+           return "Yes";
+        else
+           return "No";
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const t = readLine();
+
+    const k = parseInt(readLine(), 10);
+
+    let result = appendAndDelete(s, t, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
