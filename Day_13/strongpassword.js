a21f284e492ed97a403260cf8bff0e7e5b8d1c10
Dinesh Rajineni <dinesh.r@tadigital.com>
2018-10-10 18:19:49 +0530

    Added JavaScript Source Code for Challenge Strong Password

---
+++ Day_13/strongpassword.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumNumber function below.
+function minimumNumber(n, password) {
+    // Return the minimum number of characters to make the password strong
+    
+     const c = [/[0-9]/, /[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+"]/]
+            .map(r => !r.test(password))
+            .filter(Boolean).length
+    
+    return Math.max(c,6-n);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const password = readLine();
+
+    let answer = minimumNumber(n, password);
+
+    ws.write(answer + "\n");
+
+    ws.end();
+}
